555d37853cf096a20553db52e5e0cb59d7ba07d5
Axel <axel.fischer@fam-fis.de>
2025-06-14 23:18:48 +0200

    add install script and remake dockerfile

---
+++ entrypoint.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -x
+# Prüfen, ob HOME_DIR leer ist (kein Inhalt)
+if [ -z "$(ls -A "$HOME_DIR" 2>/dev/null)" ]; then
+  echo "Verzeichnis $HOME_DIR ist leer. Lade und entpacke Server-Paket..."
+
+  # Download
+  curl -L "https://downloads.gtnewhorizons.com/ServerPacks/GT_New_Horizons_${GREGTECH_VERSION}_Server_Java_17-21.zip" -o server.zip
+
+  # Entpacken nach HOME_DIR
+  unzip server.zip -d "$HOME_DIR"
+
+  # Startscript ausführbar machen
+  chmod +x "$HOME_DIR/startserver-java9.sh"
+
+  # Zip löschen
+  rm server.zip
+else
+  echo "Verzeichnis $HOME_DIR ist nicht leer, überspringe Download und Entpacken."
+fi
+
+#setze eula.txt
+if [[ "$EULA" = "true" ]]; then
+  echo "eula=true" >"${HOME_DIR}"/eula.txt
+else
+  echo "you have to accept EULA to install."
+  exit 10
+fi
+
+# Server starten
+./startserver-java9.sh || {
+  echo "Fehler beim Ausführen von startserver-java9.sh"
+  exit 11
+}
+
+exit 0
